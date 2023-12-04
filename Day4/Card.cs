40129d7cb261ed4aa10101ed66c365d29c2d97b7
Sindre Vingen <sindre@source.no>
2023-12-04 10:43:38 +0100

    Day4 Part1

---
+++ Day4/Card.cs
@@ -0,0 +1,51 @@
+﻿using AdventOfCode2023.Common.Extensions;
+
+namespace AdventOfCode2023.Day4;
+
+internal class Card
+{
+    public int Id { get; set; }
+
+    public string RawLine { get; set; }
+
+    public List<int> WinningNumbers { get; set; }
+
+    public List<int> CardNumbers { get; set; }
+
+    public Card(string cardLine)
+    {
+        // CardLine has this format:
+        // Card X: 1 2 3 4 5 | 1 2 3 4 5 6 7 8
+        // X = Id, Left of *pipe* are winning numbers, right of *pipe* are card numbers
+        RawLine = cardLine;
+        
+        var cardIdSplit = RawLine.Split(':');
+        Id = cardIdSplit[0].GetFirstNumber() ??
+            throw new InvalidOperationException($"No Card id found in line: {RawLine}");
+
+        var numberSplit = cardIdSplit[1].Split('|');
+        WinningNumbers = numberSplit[0].Split(' ')
+            .Where(s => !string.IsNullOrEmpty(s))
+            .Select(int.Parse)
+            .ToList();
+        
+        CardNumbers = numberSplit[1].Split(' ')
+            .Where(s => !string.IsNullOrEmpty(s))
+            .Select(int.Parse)
+            .ToList();
+    }
+
+    public int GetPoints()
+    {
+        var winningNumbers = GetWinningNumbers();
+        if (winningNumbers.Count == 0)
+        {
+            return 0;
+        }
+
+        return (int)Math.Pow(2, winningNumbers.Count - 1);
+    }
+
+    public List<int> GetWinningNumbers() =>
+        CardNumbers.Intersect(WinningNumbers).ToList();
+}
---
+++ Day4/Program.cs
@@ -0,0 +1,31 @@
+﻿using AdventOfCode2023.Common.Extensions;
+
+namespace AdventOfCode2023.Day4;
+
+public class Program
+{
+    public static void Main() { }
+
+    public static void Part1(string[] cardLines)
+    {
+        var cards = cardLines.Select(cl => new Card(cl))
+            .ToList();
+
+        foreach(var card in cards)
+        {
+            var cardPoints = card.GetPoints();
+            Console.WriteLine($"{card.RawLine} (Points: {cardPoints}) {(cardPoints > 0 ? $"(Numbers: {string.Join(", ", card.GetWinningNumbers())})" : string.Empty)}");
+        }
+
+        var points = cards.Select(c => c.GetPoints())
+            .Aggregate((sum, val) => sum + val);
+
+        Console.WriteLine();
+        Console.WriteLine($"Total points: {points}");
+    }
+
+    public static void Part2(string[] cardLines)
+    {
+        
+    }
+}
