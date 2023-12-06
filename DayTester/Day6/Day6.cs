94bc62cc0472675db2f670025bc89908196ef5ea
Sindre Vingen <sindrevingen@gmail.com>
2023-12-06 10:39:12 +0100

    Day6

---
+++ DayTester/Day6/Day6.cs
@@ -0,0 +1,33 @@
+﻿namespace DayTester;
+
+[TestClass]
+public class Day6
+{
+    [TestMethod]
+    public void Part1RunWithTestData()
+    {
+        var values = File.ReadAllLines("./Day6/Part1TestData.txt");
+        AdventOfCode2023.Day6.Program.Part1(values);
+    }
+
+    [TestMethod]
+    public void Part1Run()
+    {
+        var values = File.ReadAllLines("./Day6/Part1Data.txt");
+        AdventOfCode2023.Day6.Program.Part1(values);
+    }
+
+    [TestMethod]
+    public void Part2RunWithTestData()
+    {
+        var values = File.ReadAllLines("./Day6/Part2TestData.txt");
+        AdventOfCode2023.Day6.Program.Part2(values);
+    }
+
+    [TestMethod]
+    public void Part2Run()
+    {
+        var values = File.ReadAllLines("./Day6/Part2Data.txt");
+        AdventOfCode2023.Day6.Program.Part2(values);
+    }
+}
