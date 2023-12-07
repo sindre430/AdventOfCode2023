6e7706d1cf9474521f4361e6a4e03de366c5e020
Sindre <sindrevingen@gmail.com>
2023-12-07 19:42:20 +0100

    Day6 (#5)

---
+++ Day6/Program.cs
@@ -0,0 +1,55 @@
+﻿using AdventOfCode2023.Common.Extensions;
+
+namespace AdventOfCode2023.Day6;
+
+public class Program
+{
+    public static void Main() { }
+
+    public static void Part1(string[] raceInfo)
+    {
+        // Get time and distance info lines
+        var timeInfoLine = raceInfo.FirstOrDefault(l => l.StartsWith("Time:")) ??
+            throw new Exception("Time info not found");
+        var distanceInfoLine = raceInfo.FirstOrDefault(l => l.StartsWith("Distance:")) ??
+            throw new Exception("Distance info not found");
+       
+        // Get time and distance numbers
+        var timeNumbers = timeInfoLine.GetAllNumbersLong();
+        var distanceNumbers = distanceInfoLine.GetAllNumbersLong();
+        if(timeNumbers.Count != distanceNumbers.Count)
+        {
+            throw new Exception("Time and Distance numbers count mismatch");
+        }
+
+        // Create Race objects
+        var races = new List<Race>();
+        for(var i=0; i<timeNumbers.Count; i++)
+        {
+            races.Add(new Race
+            {
+                Id = i,
+                TimeInMs = timeNumbers[i],
+                RecordDistanceInMm = distanceNumbers[i]
+            });
+        }
+
+        // Get winning options
+        var winningOptionsCombinationCount = 1;
+        foreach(var race in races)
+        {
+            var winningOptions = race.GetWinningOptions();
+            winningOptionsCombinationCount *= winningOptions.Count;
+            Console.WriteLine($"Race {race.Id}: {winningOptions.Count} Winning Options");
+        }
+
+        // Print out the results
+        Console.WriteLine();
+        Console.WriteLine($"Winning Options Combination Count: {winningOptionsCombinationCount}");
+    }
+
+    public static void Part2(string[] raceInfo)
+    {
+        Part1(raceInfo);
+    }
+}
---
+++ Day6/Race.cs
@@ -0,0 +1,28 @@
+﻿namespace AdventOfCode2023.Day6;
+
+internal class Race
+{
+    public int Id { get; set; }
+
+    public long TimeInMs { get; set; }
+
+    public long RecordDistanceInMm { get; set; }
+
+    public List<int> GetWinningOptions()
+    {
+        var winningOptions = new List<int>();
+        for(var ms=0; ms<=TimeInMs; ms++)
+        {
+            var distance = CalculateDistance(ms);
+            if(distance > RecordDistanceInMm)
+            {
+                winningOptions.Add(ms);
+            }
+        }
+
+        return winningOptions;
+    }
+
+    private long CalculateDistance(int pushTimeInMs) =>
+        pushTimeInMs * (TimeInMs - pushTimeInMs);
+}
