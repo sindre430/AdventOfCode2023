5868ca13dbfb4a16cc4447c4560fd29d4e05a094
Sindre <sindrevingen@gmail.com>
2023-12-09 19:55:14 +0100

    Day 9 (#8)

---
+++ Day9/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace AdventOfCode2023.Day9;
+
+public class Program
+{
+    public static void Main() { }
+
+    public static void Part1(string[] mapLines)
+    {
+        List<Sequence> sequences = [];
+        foreach (var line in mapLines)
+        {
+            sequences.Add(new Sequence(line));
+        }
+
+        var sumAllNextPredictions = sequences.Sum(s => s.PredictNextValue());
+        Console.WriteLine($"Sum of all next predictions: {sumAllNextPredictions}");
+    }
+
+    public static void Part2(string[] mapLines)
+    {
+        List<Sequence> sequences = [];
+        foreach (var line in mapLines)
+        {
+            sequences.Add(new Sequence(line));
+        }
+
+        var sumAllPrevPredictions = sequences.Sum(s => s.PredictPrevValue());
+        Console.WriteLine($"Sum of all prev predictions: {sumAllPrevPredictions}");
+    }
+}
