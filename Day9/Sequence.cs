788fac276c5b7aa4486bb12f0ac2a543f361fc4d
Sindre <sindrevingen@gmail.com>
2023-12-09 19:52:57 +0100

    Day 9

---
+++ Day9/Sequence.cs
@@ -0,0 +1,37 @@
+﻿using AdventOfCode2023.Common.Extensions;
+
+namespace AdventOfCode2023.Day9;
+
+internal class Sequence(List<int> numbers)
+{
+    public List<int> Numbers { get; set; } = numbers;
+    public Sequence? Child { get; set; } = numbers.All(n => n == 0) ? null :
+            new Sequence(GenerateChildSequence(numbers));
+
+    public Sequence(string rawLine) : 
+        this(rawLine.GetAllNumbers()) { }
+
+    public int PredictNextValue()
+    {
+        var childValue = Child?.PredictNextValue() ?? 0;
+        return Numbers.Last() + childValue;
+    }
+
+    public int PredictPrevValue()
+    {
+        var childValue = Child?.PredictPrevValue() ?? 0;
+        return Numbers.First() - childValue;
+    }
+
+    public static List<int> GenerateChildSequence(List<int> numbers)
+    {
+        var res = new List<int>();
+
+        for (var i=0; i<numbers.Count-1; i++)
+        {
+            res.Add(numbers[i+1] - numbers[i]);
+        }
+
+        return res;
+    }
+}
---
+++ DayTester/Day9/Day9.cs
@@ -0,0 +1,33 @@
+﻿namespace DayTester;
+
+[TestClass]
+public class Day9
+{
+    [TestMethod]
+    public void Part1RunWithTestData()
+    {
+        var values = File.ReadAllLines("./Day9/TestData.txt");
+        AdventOfCode2023.Day9.Program.Part1(values);
+    }
+
+    [TestMethod]
+    public void Part1Run()
+    {
+        var values = File.ReadAllLines("./Day9/Data.txt");
+        AdventOfCode2023.Day9.Program.Part1(values);
+    }
+
+    [TestMethod]
+    public void Part2RunWithTestData()
+    {
+        var values = File.ReadAllLines("./Day9/TestData.txt");
+        AdventOfCode2023.Day9.Program.Part2(values);
+    }
+
+    [TestMethod]
+    public void Part2Run()
+    {
+        var values = File.ReadAllLines("./Day9/Data.txt");
+        AdventOfCode2023.Day9.Program.Part2(values);
+    }
+}
