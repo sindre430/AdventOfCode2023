a35ec7ddfb597cfbd050b5d0e39086164f8e4d6f
Sindre <sindrevingen@gmail.com>
2023-12-13 22:07:32 +0100

    Day11 (#10)

--- Common/Classes/Position.cs
+++ Common/Classes/Position.cs
@@ -5,4 +5,8 @@ public class Position<T>(T x, T y)
     public T X { get; set; } = x;
 
     public T Y { get; set; } = y;
+
+    public T ManhattanDistance(Position<T> position) =>
+        Math.Abs((dynamic) X - (dynamic) position.X) +
+        Math.Abs((dynamic) Y - (dynamic) position.Y);
 }
